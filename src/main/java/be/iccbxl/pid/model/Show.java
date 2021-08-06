96c7ae1ca805d19a855198cd1023d8e90274191e
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-08-06 08:50:39 +0200

    correction import OneToMany

--- src/main/java/be/iccbxl/pid/model/Show.java
+++ src/main/java/be/iccbxl/pid/model/Show.java
@@ -1,5 +1,6 @@
 package be.iccbxl.pid.model;
 
+import java.util.*;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
@@ -8,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Column;
 
