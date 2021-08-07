eca196df287c40f11072fc1d213ac88807f3072e
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-08-07 14:56:34 +0200

    import role.java + modification affichage show/id

--- src/main/java/be/iccbxl/pid/model/Role.java
+++ src/main/java/be/iccbxl/pid/model/Role.java
@@ -1,9 +1,14 @@
 package be.iccbxl.pid.model;
 
+import java.util.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 
