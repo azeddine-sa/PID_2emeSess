70ec9aaa8a375e9207e78331aadeda738a774c56
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-08-07 18:08:34 +0200

    Form update Artist

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -3,6 +3,7 @@ package be.iccbxl.pid.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import be.iccbxl.pid.model.Artist;
 import be.iccbxl.pid.model.ArtistService;
@@ -61,6 +63,49 @@ public class ArtistController {
 		service.addArtist(artist);
 	    
 	    return "redirect:/artists/"+artist.getId();
+	}
+
+	@GetMapping("/artists/{id}/edit")
+	public String edit(Model model, @PathVariable("id") String id, HttpServletRequest request) {
+		Artist artist = service.getArtist(id);
+
+		model.addAttribute("artist", artist);
+
+
+		//Générer le lien retour pour l'annulation
+	String referrer = request.getHeader("Referer");
+
+		if(referrer!=null && !referrer.equals("")) {
+			model.addAttribute("back", referrer);
+		} else {
+			model.addAttribute("back", "/artists/"+artist.getId());
+		}
+		
+		return "artist/edit";
+	}
+	
+	@PutMapping("/artists/{id}/edit")
+	public String update(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, @PathVariable("id") String id, Model model) {
+	    
+		if (bindingResult.hasErrors()) {
+			return "artist/edit";
+		}
+		
+		Artist existing = service.getArtist(id);
+		
+		if(existing==null) {
+			return "artist/index";
+		}
+		
+		Long indice = (long) Integer.parseInt(id);
+
+		artist.setId(indice);
+
+		service.updateArtist(existing.getId(), artist);
+	    
+		model.addAttribute("artist", artist);
+	    
+		return "redirect:/artists/"+artist.getId();
 	}
 
 }
