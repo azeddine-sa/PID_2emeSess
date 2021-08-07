ecb30f27ee45df9dab3fba1174e9f6421107d257
bennour Aymen <bennour.ma@gmail.com>
2021-08-07 21:17:10 +0200

    Form create Artist

--- src/main/java/be/iccbxl/pid/controller/TypeController.java
+++ src/main/java/be/iccbxl/pid/controller/TypeController.java
@@ -3,11 +3,16 @@ package be.iccbxl.pid.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import be.iccbxl.pid.model.Type;
 import be.iccbxl.pid.model.TypeService;
@@ -36,5 +41,26 @@ public class TypeController {
 		
         return "type/show";
     }
+
+	@GetMapping("/artists/create")
+	public String create(Model model) {
+	    Artist artist = new Artist(null,null);
+
+	    model.addAttribute("artist", artist);
+		
+	    return "artist/create";
+	}
+	
+	@PostMapping("/artists/create")
+	public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model) {
+	    
+	    if (bindingResult.hasErrors()) {
+		return "artist/create";
+	    }
+		    
+	    service.addArtist(artist);
+	    
+	    return "redirect:/artists/"+artist.getId();
+	}
 
 }
