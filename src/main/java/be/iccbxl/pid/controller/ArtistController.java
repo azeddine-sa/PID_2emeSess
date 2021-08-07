6bb9d183f9bcfce6c26afe9cea0abcbb8092e9de
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-08-07 17:44:51 +0200

    Form create Artist + modification stratergy Artist de AUTO en IDENTITY + app.prop empecher hibernate de rechercher des entrés hibernate_sequence

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -3,11 +3,16 @@ package be.iccbxl.pid.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import be.iccbxl.pid.model.Artist;
 import be.iccbxl.pid.model.ArtistService;
@@ -36,5 +41,26 @@ public class ArtistController {
 		
         return "artist/show";
     }
+
+	@GetMapping("/artists/create")
+    public String create(Model model) {
+		Artist artist = new Artist(null,null);
+
+	    model.addAttribute("artist", artist);
+		
+	    return "artist/create";
+    }
+
+	@PostMapping("/artists/create")
+	public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model) {
+	    
+	    if (bindingResult.hasErrors()) {
+			return "artist/create";
+	    }
+		
+		service.addArtist(artist);
+	    
+	    return "redirect:/artists/"+artist.getId();
+	}
 
 }
