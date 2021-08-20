e495ec9960235a00ef2ef1ddad95d1bab8c84987
Saouti Azeddine <saouti.azeddine@gmail.com>
2021-08-20 12:57:45 +0200

    step1

--- src/main/java/be/iccbxl/pid/controller/HelloController.java
+++ src/main/java/be/iccbxl/pid/controller/HelloController.java
@@ -1,14 +1,20 @@
 package be.iccbxl.pid.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1> Hi Admin! </h1>");
     }
 
 }
---
+++ src/main/java/be/iccbxl/pid/security/UserDetailsImpl.java
@@ -0,0 +1,64 @@
+package be.iccbxl.pid.security;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import be.iccbxl.pid.model.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRoles().get(0).getRole());
+        authorities.add(authority);
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return user.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+    
+}
